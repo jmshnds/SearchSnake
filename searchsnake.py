0f933daf40f509b1345eebe93f751e412a72f3e4
James <jmshnds@gmail.com>
2018-06-13 09:35:00 -0700

    Update variables names

--- searchsnake.py
+++ searchsnake.py
@@ -8,21 +8,22 @@ if __name__ == '__main__':
 	snakePos = (2, 0)
 	foodPos = (3, 2)
 	board = []
-	n = 5
-	for _ in range(n):
-		board.append([0]*n)
-	print(board)
+	board_size = 5
+	for _ in range(board_size):
+		board.append([0]*board_size)
 
 	# Set up gamestate and search problem
 	game = GameState(snakePos, foodPos, board)
 	problem = PositionSearchProblem(game)
 
 	# Get resulting path nodes from performing a search
-	nodes = search.breadthFirstSearch(problem)
-	for node in nodes:
+	path_nodes = search.breadthFirstSearch(problem)
+
+	# Print path nodes
+	for node in path_nodes:
 		print(node)
 
-	actions = [node.action for node in nodes]
+	actions = [node.action for node in path_nodes]
 	for action in actions[1:]:
 		# Draw game board
 		for row in game.board:
@@ -33,6 +34,6 @@ if __name__ == '__main__':
 
 		input("Press enter to continue...")
 
-	# Draw game board one last time
+	# Draw game board one last time (to see win state)
 	for row in game.board:
 		print(row)
